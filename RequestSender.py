385808ed656354e858b69faf33eb2181a274e168
dungwoong <78326933+dungwoong@users.noreply.github.com>
2022-05-29 18:53:27 -0600

    Create RequestSender.py

---
+++ RequestSender.py
@@ -0,0 +1,42 @@
+from typing import List
+import requests
+import time
+from dotenv import load_dotenv
+import os
+
+
+class RequestFailedException(Exception):
+    pass
+
+
+# make dictionary of variables, then do <VARIABLE_NAME> in your url. Sender will
+# send a regular request, but replace your variables before sending the request to the URL
+def send_request(url: str, variables=dict(), headers=dict(), method="GET") -> requests.Response:
+    uarel = url
+    # get api key or something
+    url = process_url(url, variables)
+    # send the req
+    if method == "GET":
+        response = requests.get(url=url, headers=headers)
+        # Logger.verbose("sending GET to " + url)
+    else:
+        response = requests.post(url=url, headers=headers)
+        # Logger.verbose("sending POST to " + url)
+
+    if not response.ok:
+        raise RequestFailedException("Failed " + url + ", code " + str(response.status_code))
+    return response
+
+
+def process_url(url, variables):
+    if "<API_KEY>" in url:
+        key = get_api_key()
+        url = url.replace("<API_KEY>", key)
+    for key in variables:
+        url = url.replace("<" + key + ">", variables[key])
+    return url
+
+
+def get_api_key():
+    load_dotenv()
+    return os.getenv('APIKEY')
